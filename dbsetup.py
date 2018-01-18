3efb026ce028eab725e19358c5c63686c2945873
Rehan Azher <rehan.azher@gmail.com>
2018-01-17 18:05:24 -0800

    Map, Form with CSS

--- dbsetup.py
+++ dbsetup.py
@@ -2,23 +2,30 @@ import pymysql
 import dbconfig
 connection = pymysql.connect(host='localhost', user=dbconfig.db_user,passwd=dbconfig.db_passwd)
 try:
-	with connection.cursor as cursor
+	cursor =  connection.cursor()
 	sql = "CREATE DATABASE IF NOT EXISTS CRIMEAPP"
-	cursor.execute(sql)
-	sql = """CREATE TABLE IF NOT EXISTS crimemap.crimes (
-		id int NOT NULL AUTO_INCREMENT,
-		latitude FLOAT(10,6),
-		longitude FLOAT(10,6),
-		date DATETIME,
-		category VARCHAR(50),
-		description VARCHAR(1000),
-		updated_at TIMESTAMP,
-		PRIMARY KEY (id)
-		)"""
+	try: 
+		cursor.execute(sql)
+	except MySQLdb.Warning as e:
+		print ("Mysql Warning ")
+	except Warning as e:
+		print ("Mysql Warning ")
+
+	sql = """CREATE TABLE IF NOT EXISTS CRIMEAPP.crimes (
+			id int NOT NULL AUTO_INCREMENT,
+			latitude FLOAT(10,6),
+			longitude FLOAT(10,6),
+			date DATETIME,
+			category VARCHAR(50),
+			description VARCHAR(1000),
+			updated_at TIMESTAMP,
+			PRIMARY KEY (id)
+			)"""
 	cursor.execute(sql)
 	connection.commit()
 finally:
+	cursor.close()
 	connection.close()
-	
+
 
 
